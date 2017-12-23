b5c835f2eced5f8f5bde41dafeb462585682b562
Sabrina Warner <33816901+wasabsi@users.noreply.github.com>
2017-12-23 12:03:49 -0500

    Update NullBinaryNodeException.java

--- NullBinaryNodeException.java
+++ NullBinaryNodeException.java
@@ -3,14 +3,11 @@ package edu.cofc.csci230;
 /**
  * 
  * Exception used in binary tree / binary search tree 
- * coding assignment.
  * 
  * This exception is thrown when a null binary node, or 
  * a binary node with a null element is added to the 
  * binary tree or binary search tree.
  * 
- * @author CSCI 230: Data Structures and Algorithms Fall 2017
- *
  */
 @SuppressWarnings("serial")
 public class NullBinaryNodeException extends Exception {
